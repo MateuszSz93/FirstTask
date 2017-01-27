4df9103cf773d4baab6101c52fbeffaf0071cbbd
MateuszSz93 <spens16@gmail.com>
2017-01-27 01:08:18 +0100

    Improved error handling.

--- src/main/java/pl/mszkwarkowski/controller/ExceptionHandlerAdvice.java
+++ src/main/java/pl/mszkwarkowski/controller/ExceptionHandlerAdvice.java
@@ -1,6 +1,7 @@
 package pl.mszkwarkowski.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.orm.jpa.JpaObjectRetrievalFailureException;
 import org.springframework.web.bind.annotation.*;
 import pl.mszkwarkowski.other.Error;
@@ -9,43 +10,22 @@ import javax.ws.rs.*;
 
 @RestControllerAdvice
 public class ExceptionHandlerAdvice {
-
     /**
-     * This method returns information when "BadRequest" exception occurs.
-     *
-     * @param ex BadRequestException.
-     * @return Error object.
-     */
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(BadRequestException.class)
-    public Error displayBadRequestMessage(BadRequestException ex) {
-        Error error = new Error(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase(), ex.getMessage());
-        return error;
-    }
-
-    /**
-     * This method returns information when "Forbidden" exception occurs.
-     *
-     * @param ex ForbiddenException.
-     * @return Error object.
-     */
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    @ExceptionHandler(ForbiddenException.class)
-    public Error displayForbiddenMessage(ForbiddenException ex) {
-        Error error = new Error(HttpStatus.FORBIDDEN.value(), HttpStatus.FORBIDDEN.getReasonPhrase(), ex.getMessage());
-        return error;
-    }
-
-    /**
-     * This method returns information when "JpaObjectRetrievalFailure" or "NotFoundException" exception occurs.
+     * This method returns information when "JpaObjectRetrievalFailure", "NotFound", "Forbidden" or "BadRequest" exception occurs.
      *
      * @param ex Exception.
      * @return Error object.
      */
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler({JpaObjectRetrievalFailureException.class, NotFoundException.class})
-    public Error displayNotFoundMessage(Exception ex) {
-        Error error = new Error(HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND.getReasonPhrase(), ex.getMessage());
-        return error;
+    @ExceptionHandler({JpaObjectRetrievalFailureException.class, NotFoundException.class, ForbiddenException.class, BadRequestException.class})
+    public ResponseEntity<Error> displayExceptionData(Exception ex) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        if (ex.getClass() == BadRequestException.class){
+            status = HttpStatus.BAD_REQUEST;
+        }
+        if (ex.getClass() == ForbiddenException.class){
+            status = HttpStatus.FORBIDDEN;
+        }
+        Error error = new Error(status.value(), status.getReasonPhrase(), ex.getMessage());
+        return ResponseEntity.status(status).body(error);
     }
 }
